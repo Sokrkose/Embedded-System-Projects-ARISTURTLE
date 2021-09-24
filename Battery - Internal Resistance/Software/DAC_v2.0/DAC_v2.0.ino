7af7de3c49afcc0b68ff9c5aefa3c3ddc12efa11
Sokrkose <sokkose@gmail.com>
2021-09-24 13:19:27 +0300

    Add files via upload

---
+++ Battery - Internal Resistance/Software/DAC_v2.0/DAC_v2.0.ino
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <LiquidCrystal.h>
+#include "LTC2630.h"
+
+LTC2630 dac;
+
+const int rs = 35, en = 33, d4 = 32, d5 = 31, d6 = 30, d7 = 15;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+uint8_t V_control = 61;  // choose DAC Voltage Output
+int V_ctrl = (int ) V_control;
+float a = (float ) V_ctrl/255;
+float V = a*4.97;
+float Id = V/0.150;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(53, OUTPUT);
+  
+  //dac power down and then set Vref = Vcc = 5V
+  dac.init();
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+}
+
+void loop() {
+
+  //Write and Update Command and then set Vout = DAC_voltage
+  dac.backlight_set(V_control);
+  lcd.setCursor(0, 0);
+  lcd.print("V=");
+  lcd.setCursor(2, 0);
+  lcd.print(V);
+  lcd.setCursor(7, 0);
+  lcd.print("I=");
+  lcd.setCursor(9,0);
+  lcd.print(Id);
+
+  
+  delay(3000);
+
+  //Vout = 0 (maybe not needed)
+  dac.backlight_set(BACKLIGHT_FULL_OFF);
+  //DAC Power Off
+  dac.cmd_send(DAC_POWER_DOWN, 0);
+ 
+  delay(1000);
+}
---
+++ Battery - Internal Resistance/Software/Internal_Resistance_PCB_code_v1.0/Internal_Resistance_PCB_code_v1.0.ino
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include "LTC2630.h"
+#include "ADCCONVERT.h"
+
+LTC2630 dac;
+ADCCONVERT adc;
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+uint8_t DAC_input = 100;
+int CS2 = 10;
+int V_load;
+int V_OC;
+int button_pin;
+
+void setup() {
+  
+  Serial.begin(9600);
+  Serial.println("Setup Begin");
+
+  lcd.begin(16, 2);
+  lcd.print("Waiting");
+
+  V_OC = adc.adcConvert(0x00, CS2);
+  
+  dac.init();
+
+  Serial.println("Setup End");
+
+}
+
+void loop() {
+  
+  if(digitalRead(button_pin) == 0){
+        int V_batt[500];                          //1000 samples
+        dac.backlight_set(DAC_input);             //Sets DAC output voltage... V_control
+        V_load = adc.adcConvert(0x01, CS2);       //Monitor Load Voltage... V_load
+
+        lcd.setCursor(0,0);
+        lcd.write("I = ");
+        lcd.print(V_load/150);
+        
+        if(V_load - (DAC_input/4096)*5 <= 0.05  || V_load - (DAC_input/4096)*5 >= 0.05 ){    // if (V_load == V_Control)
+              for(int index = 0; index < 500; index++){
+                V_batt[index] = adc.adcConvert(0x00, CS2);
+                Serial.println(V_batt[index]);
+                delay(1);
+              }
+              Serial.print("\n");
+        }
+
+        //Vout = 0 (maybe not needed)
+        dac.backlight_set(0);
+        //DAC Power Off
+        dac.cmd_send(DAC_POWER_DOWN, 0);
+
+        int min_voltage = V_batt[0];
+        for(int index = 1; index < 500; index++){
+          if(V_batt[index] < min_voltage){
+            min_voltage = V_batt[index];
+          }
+        }
+        Serial.print(min_voltage);
+        
+        int res = (V_OC - min_voltage)/(V_load/150);
+
+        lcd.setCursor(0,1);
+        lcd.write("Int Res = ");
+        lcd.print(res);
+  
+  }  
+
+}
+
